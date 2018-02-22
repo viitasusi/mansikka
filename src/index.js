9ff675f0833628b4f1e6cf34d0b5ff2554c4724e
viitasusi <vllhtnn@gmail.com>
2018-02-22 21:48:22 +0200

    fixed a tiny bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
+import './css/index.css';
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
